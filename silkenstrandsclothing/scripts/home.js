bf31eda4388e75b820a9670d1a0e467cc5c27863
soape-cathcart <soape.cathcart.shaune@gmail.com>
2022-08-08 06:52:50 -0500

    home page responsive

--- silkenstrandsclothing/scripts/home.js
+++ silkenstrandsclothing/scripts/home.js
@@ -41,7 +41,7 @@ jQuery('document').ready(function () {
       autoplay: true,
       dots: false,
       infinite: true,
-      autoplaySpeed: 2500,
+      autoplaySpeed: 1500,
       speed: 2000,
       fade: true,
       cssEase: 'ease-in-out',
@@ -68,123 +68,124 @@ jQuery('document').ready(function () {
    });
 
    // Shuffle function
-   function shuffle(array) {
-    let currentIndex = array.length,  randomIndex;
-    while (currentIndex != 0) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
-    }
-    return array;
-  }
+  //  function shuffle(array) {
+  //   let currentIndex = array.length,  randomIndex;
+  //   while (currentIndex != 0) {
+  //     randomIndex = Math.floor(Math.random() * currentIndex);
+  //     currentIndex--;
+  //     [array[currentIndex], array[randomIndex]] = [
+  //       array[randomIndex], array[currentIndex]];
+  //   }
+  //   return array;
+  // }
  
 //  Preview Section
-   const settings = {
-      "async": true,
-      "crossDomain": true,
-      "url": "https://app.ecwid.com/api/v3/11704047/products?enabled=true&token=public_e3zBkEmPUJQfruPyHm3e7bw5kgHLwRUx",
-      "method": "GET",
-      "headers": {
-        "Accept": "application/json"
-      }
-    };
-    
-    jQuery.ajax(settings).done(function (response) {
-       var previews = jQuery('.preview-list')
-       var items = response.items
 
-       console.log(items)
+// Now using plugin
+
+  //  const settings = {
+  //     "async": true,
+  //     "crossDomain": true,
+  //     "url": "https://app.ecwid.com/api/v3/11704047/products?enabled=true&token=public_e3zBkEmPUJQfruPyHm3e7bw5kgHLwRUx",
+  //     "method": "GET",
+  //     "headers": {
+  //       "Accept": "application/json"
+  //     }
+  //   };
+    
+    // jQuery.ajax(settings).done(function (response) {
+    //    var previews = jQuery('.preview-list')
+    //    var items = response.items
+
+      //  console.log(items)
+
       // Some kind of hover card flip effect to show the reverse shots
        //  FIX URLS - ADD /store.html BEFORE '#!'
-       var urls = items.map(function(e) {
-            return e.url;
-       })
-       var imgs = items.map(function(e) {
-         return e.thumbnailUrl;
-       })
-       var reverseImgs = items.map(function(e) {
-         return e.media.images[1].imageOriginalUrl;
-       })
-       var allImgs = imgs.concat(reverseImgs);
-       shuffle(imgs)
-       shuffle(imgs)
-       shuffle(reverseImgs)
-       shuffle(reverseImgs)
+      //  var urls = items.map(function(e) {
+      //       return e.url;
+      //  })
+      //  var imgs = items.map(function(e) {
+      //    return e.thumbnailUrl;
+      //  })
+      //  var reverseImgs = items.map(function(e) {
+      //    return e.media.images[1].imageOriginalUrl;
+      //  })
+      //  var allImgs = imgs.concat(reverseImgs);
+      //  shuffle(imgs)
+      //  shuffle(imgs)
+      //  shuffle(reverseImgs)
+      //  shuffle(reverseImgs)
 
 
-         for (var i=0; i<allImgs.length; i++) {
-          previews.append(`<img src="${imgs[i]}" width="80%" />`);
-          previews.append(`<img src="${reverseImgs[i]}" width="80%" />`);
-        }
+        //  for (var i=0; i<allImgs.length; i++) {
+        //   previews.append(`<img src="${imgs[i]}" width="80%" />`);
+        //   previews.append(`<img src="${reverseImgs[i]}" width="80%" />`);
+        // }
 
-       previews.slick({
-         autoplay: true,
-         arrows: false,
-         infinite: true,
-         slidesToShow: 10,
-         slidesToScroll: 1,
-         dots: false,
-         pauseOnHover: false,
-         cssEase: 'linear',
-         autoplaySpeed: .001,
-         speed: 5000,
-         responsive: [
-          {
-            breakpoint: 1670,
-            settings: {
-              slidesToShow: 9
-            }
-          },
-          {
-            breakpoint: 1550,
-            settings: {
-              slidesToShow: 8
-            }
-          },
-          {
-            breakpoint: 1350,
-            settings: {
-              slidesToShow: 7
-            }
-          },
-          {
-            breakpoint: 1200,
-            settings: {
-              slidesToShow: 6
-            }
-          },
-          {
-            breakpoint: 1050,
-            settings: {
-              slidesToShow: 5
-            }
-          },
-          {
-            breakpoint: 875,
-            settings: {
-              slidesToShow: 4
-            }
-          },
-          {
-            breakpoint: 700,
-            settings: {
-              slidesToShow: 3
-            }
-          },
-          {
-            breakpoint: 525,
-            settings: {
-              slidesToShow: 2
-            }
-          }
-          // You can unslick at a given breakpoint now by adding:
-          // settings: "unslick"
-          // instead of a settings object
-        ]
-       });
+      //  previews.slick({
+      //    autoplay: true,
+      //    arrows: false,
+      //    infinite: true,
+      //    slidesToShow: 10,
+      //    slidesToScroll: 1,
+      //    dots: false,
+      //    pauseOnHover: false,
+      //    cssEase: 'linear',
+      //    autoplaySpeed: .001,
+      //    speed: 5000,
+      //    responsive: [
+      //     {
+      //       breakpoint: 1670,
+      //       settings: {
+      //         slidesToShow: 9
+      //       }
+      //     },
+      //     {
+      //       breakpoint: 1550,
+      //       settings: {
+      //         slidesToShow: 8
+      //       }
+      //     },
+      //     {
+      //       breakpoint: 1350,
+      //       settings: {
+      //         slidesToShow: 7
+      //       }
+      //     },
+      //     {
+      //       breakpoint: 1200,
+      //       settings: {
+      //         slidesToShow: 6
+      //       }
+      //     },
+      //     {
+      //       breakpoint: 1050,
+      //       settings: {
+      //         slidesToShow: 5
+      //       }
+      //     },
+      //     {
+      //       breakpoint: 875,
+      //       settings: {
+      //         slidesToShow: 4
+      //       }
+      //     },
+      //     {
+      //       breakpoint: 700,
+      //       settings: {
+      //         slidesToShow: 3
+      //       }
+      //     },
+      //     {
+      //       breakpoint: 525,
+      //       settings: {
+      //         slidesToShow: 2
+      //       }
+      //     }
+      //   ]
+      //  });
 
-      });
+      // });
 
 
 
@@ -204,7 +205,7 @@ jQuery('document').ready(function () {
       jQuery('.clothing-section .left').removeClass('showing');
     }
   }, {
-    offset: '50%'
+    offset: '70%'
   }) 
 
 
@@ -217,7 +218,7 @@ jQuery('document').ready(function () {
       }
     }, 500);
     
-  }, {offset: '50%'})
+  }, {offset: '70%'})
 
 
   jQuery('.clothing-section .right').waypoint(function(direction){
@@ -226,7 +227,7 @@ jQuery('document').ready(function () {
     } else {
       jQuery('.clothing-section .right').removeClass('showing');
     }
-  }, {offset: '50%'})
+  }, {offset: '70%'})
 
 
   jQuery('.shaune').waypoint(function(direction){
@@ -243,7 +244,7 @@ jQuery('document').ready(function () {
           jQuery('.shaune img').removeClass('showing');
         }
       }, 500)
-  }, {offset: '50%'})
+  }, {offset: '70%'})
 
 
   jQuery('.about-section .right').waypoint(function(direction){
@@ -260,7 +261,7 @@ jQuery('document').ready(function () {
           jQuery('.about-copy-box').removeClass('showing');
         }
       }, 500)
-  }, {offset: '50%'})
+  }, {offset: '70%'})
 
 
 
---
+++ silkenstrandsclothing/scripts/shop.js
@@ -0,0 +1,17 @@
+
+jQuery('document').ready(function () {
+
+   
+  //  if (jQuery('.post-type-archive-product')) {
+      
+      console.log('shop archive')
+      
+    //   jQuery('ul.products .product').each(function(i){
+    //    setTimeout(function(){
+    //      jQuery('.ul.products .product').eq(i).addClass('is-showing');
+    //    }, 150 * (i+1));
+    //  });
+
+  //  }
+
+});
